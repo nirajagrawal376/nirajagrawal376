30dc61288a626b19ca8fd6e56bc3706ace7c0f55
nirajagrawal376 <97953663+nirajagrawal376@users.noreply.github.com>
2022-01-18 18:41:28 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 public static void main(String[] args){
   System.out.println("Hello anime world");
-System.out.println("for a new branch"); }
+System.out.println("for a new branch");
+System.out.println("second time from the hello branch 1");}
 }
